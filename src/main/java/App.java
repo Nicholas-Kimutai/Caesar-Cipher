0201ce1eebed53698edf4d4ec0d011dcb9e17917
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-24 21:13:24 +0300

    add input method on App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
     private String text;
     private int key;
 
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Ceasar Cipher program!\n");
+        System.out.println("Select your functionality:");
+        System.out.println("1.Encoding\n2.Decoding");
+        Scanner newScanner =new Scanner(System.in);
+        int choice = newScanner.nextInt();
+
+    }
+
 
 }
 
