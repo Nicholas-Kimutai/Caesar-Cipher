3d74b97d89c71cce312b9dd484e65db4f4c9f06e
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-25 00:04:01 +0300

    Add Encoding test

--- src/test/java/EncodeTest.java
+++ src/test/java/EncodeTest.java
@@ -15,4 +15,17 @@ public class EncodeTest {
 
     }
 
+    @Test
+    public void  encode_keyBoundaryLength_exception(){
+        try {
+            Encode encode = new Encode();
+            String actual = encode.encoding("STEVE JOBS WAS ONE OF THE MOST VISIONARY TECH LEADERS",27);
+
+        }catch (Exception ex){
+            assertEquals("The key must be between 1 and 25",ex.getMessage());
+        }
+
+    }
+
+
 }
