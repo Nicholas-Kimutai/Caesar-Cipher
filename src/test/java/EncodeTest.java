d917d68c1367a6b9f7fa9d49f82df732cd475e7e
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-24 20:43:55 +0300

    add encode test cases

--- src/test/java/EncodeTest.java
+++ src/test/java/EncodeTest.java
@@ -1,2 +1,17 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class EncodeTest {
+    @Test
+    public  void encode_onlyString_exception() throws Exception {
+        //arrange
+        Encode encode = new Encode();
+
+        //act
+        String actual = encode.encoding("1",1);
+
+        //assert
+        assertTrue(actual instanceof String);
+
+    }
 }
