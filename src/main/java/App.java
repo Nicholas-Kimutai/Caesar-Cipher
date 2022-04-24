d0e2a062853ee9f3721922c04158509d6cf0a035
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-24 22:56:20 +0300

    refactor App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,14 +7,38 @@ public class App {
     private String text;
     private int key;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        System.out.println("Welcome to Ceasar Cipher program!\n");
+        System.out.println("Welcome to Caesar Cipher program!\n");
         System.out.println("Select your functionality:");
         System.out.println("1.Encoding\n2.Decoding\n");
-        Scanner newScanner =new Scanner(System.in);
+        Scanner newScanner = new Scanner(System.in);
         int selection = newScanner.nextInt();
-}
-}
+
+        if(selection==1){
+            System.out.println("Encoding");
+            newScanner.nextLine();
+            System.out.println("Input String Text: ");
+            String text = newScanner.nextLine();
+            Encode newEncryption = new Encode();
+
+            System.out.println(" Enter decoding Key:");
+            Integer key = Integer.parseInt(newScanner.nextLine());
+
+            if(key < 1 || key > 25){
+                System.out.println("The key must be between 1 and 25");
+            } else{
+                Encode encode = new Encode();
+                String encoded = encode.encoding(text,key);
+                System.out.println("Your encrypted text is: ");
+                System.out.println(encoded);
+
+            }
 
 
+        }
+
+
+    }
+
+}
