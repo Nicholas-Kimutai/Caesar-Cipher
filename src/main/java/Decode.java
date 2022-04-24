2c0d0e6b334420caa14dadba5c17c784e51b21bc
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-24 20:35:11 +0300

    update class decode

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,8 +1,39 @@
 public class Decode {
     private String decodeMessage="";
+    private String encodeMessage;
     private int key;
 
-    public void decoding()throws Exception{
+    public String decoding() throws Exception{
 
+        if(key < 1 || key > 25) throw new Exception("The key must be between 1 and 25");
+        for(int i=0; i<encodeMessage.length(); i++){
+            char alphabet=encodeMessage.charAt(i);
+            //check if alphabet is a valid letter
+            if(Character.isLetter(alphabet)){
+                //For Uppercase letters
+                if(Character.isUpperCase(alphabet)){
+                    char newAlphabet=(char)(alphabet+key);
+                    if(newAlphabet>'Z'){
+                        decodeMessage += (char)(alphabet-(26+key));
+                    }else{
+                        decodeMessage += newAlphabet;
+
+                    }
+                }
+                // For lowercase letters
+                else if(Character.isLowerCase(alphabet)){
+                    char newAlphabet=(char)(alphabet+key);
+                    if(newAlphabet>'z'){
+                        decodeMessage += (char)(alphabet -(26+key));
+                    }else{
+                        decodeMessage += newAlphabet;
+                    }
+                }
+            }else{
+                decodeMessage += alphabet;
+            }
+        }
+        return decodeMessage;
     }
+
 }
