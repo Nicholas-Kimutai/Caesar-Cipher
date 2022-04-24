fcab0c140f6e81db71d78e18d282329d0a767a52
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-24 19:14:28 +0300

    Add encoding method

--- src/main/java/Encode.java
+++ src/main/java/Encode.java
@@ -1,2 +1,10 @@
+
 public class Encode {
+
+    private String encodeMessage="";
+    private int key;
+
+    public String encoding(String plainText, int key) throws Exception{
+        int alphabet= plainText.length();
+    }
 }
